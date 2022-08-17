cbe028d68b86ff81a6ec1fe25fa5d679f504f62b
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-17 21:28:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'currentUser';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = 'Welcome, !'
+
+
+
+
+
